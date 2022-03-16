a41910005ed675a472bdcf63be7c613e64d585ee
Mehoff <mixov2000@gmail.com>
2022-03-16 15:32:48 +0200

    Wood textures. Physics tweaks

--- Assets/Scripts/Kegel.cs
+++ Assets/Scripts/Kegel.cs
@@ -5,14 +5,10 @@ using UnityEngine;
 public class Kegel : MonoBehaviour
 {
     private Rigidbody rb;
-    private Vector3 centerOfMass;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
-        centerOfMass = new Vector3(0, 0, 0);
-        rb.centerOfMass = centerOfMass;
     }
 
     void Update()
